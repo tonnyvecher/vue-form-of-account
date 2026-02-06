55971027ff15183cb61afc4fb57bc325c31667b0
tonnyvecher <sidoroffds@gmail.com>
2026-02-06 12:55:30 +0400

    feat|VFA-1(accounts.store.ts): создание стора для хранения валидных записей

--- src/stores/accounts.store.ts
+++ src/stores/accounts.store.ts
@@ -1,12 +1,31 @@
-import { ref, computed } from 'vue'
+import type { Account } from '@/shared/types/accounts'
+import { loadFromStorage, saveToStorage } from '@/shared/utils/storage'
 import { defineStore } from 'pinia'
+import { computed, ref, watch } from 'vue'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+const STORAGE_KEY = 'accounts_v1'
+
+export const useAccountsStore = defineStore('accounts', () => {
+  const accounts = ref<Account[]>(loadFromStorage<Account[]>(STORAGE_KEY, []))
+
+  const count = computed(() => accounts.value.length)
+
+  function upsert(account: Account) {
+    const idx = accounts.value.findIndex((acc) => acc.id === account.id)
+    if (idx === -1) accounts.value.push(account)
+    else accounts.value[idx] = account
   }
 
-  return { count, doubleCount, increment }
+  function remove(id: string) {
+    accounts.value = accounts.value.filter((acc) => acc.id !== id)
+  }
+
+  watch(accounts, (val) => saveToStorage(STORAGE_KEY, val), { deep: true })
+
+  return {
+    accounts,
+    count,
+    upsert,
+    remove,
+  }
 })
